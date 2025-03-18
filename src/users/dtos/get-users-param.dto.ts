dee9633afad97d6862e9da9f254c5356985ad5c4
jankacz <jankaczmarek997@gmail.com>
2025-03-18 14:10:48 +0100

    End of part 3

---
+++ src/users/dtos/get-users-param.dto.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional } from 'class-validator';
+
+export class GetUsersParamDto {
+  @IsOptional()
+  @IsInt()
+  @Type(() => Number)
+  id?: number;
+}
